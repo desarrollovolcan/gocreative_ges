4d338c76ee030ce7c84c3d55c29849cb2b5abb67
desarrollovolcan <eisla@fvolcan.cl>
2025-12-29 21:00:31 -0300

    Add CRM projects hub and modern form styling

--- partials/sidenav.php
+++ partials/sidenav.php
@@ -287,6 +287,16 @@
                 <div class="collapse" id="sidebarCRM">
                     <ul class="sub-menu">
                         <li class="side-nav-item">
+                            <a href="projects.php" class="side-nav-link">
+                                <span class="menu-text" data-lang="crm-projects-services">Projects &amp; Services</span>
+                            </a>
+                        </li>
+                        <li class="side-nav-item">
+                            <a href="tickets-list.php" class="side-nav-link">
+                                <span class="menu-text" data-lang="crm-service-desk">Service Desk</span>
+                            </a>
+                        </li>
+                        <li class="side-nav-item">
                             <a href="crm-contacts.php" class="side-nav-link">
                                 <span class="menu-text" data-lang="crm-contacts">Contacts</span>
                             </a>

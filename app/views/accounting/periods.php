f0b3cbd18be2e915eff0875af623f58ba12ebdb4
desarrollovolcan <eisla@fvolcan.cl>
2026-01-04 23:35:26 -0300

    Add code-based reopening for periods

--- app/views/accounting/periods.php
+++ app/views/accounting/periods.php
@@ -45,6 +45,20 @@
                                                 <input type="hidden" name="period_id" value="<?php echo (int)$period['id']; ?>">
                                                 <button type="submit" class="btn btn-outline-danger btn-sm">Cerrar</button>
                                             </form>
+                                        <?php else: ?>
+                                            <div class="d-flex flex-column flex-md-row gap-2 justify-content-end">
+                                                <form method="post" action="index.php?route=accounting/periods/request-open">
+                                                    <input type="hidden" name="csrf_token" value="<?php echo csrf_token(); ?>">
+                                                    <input type="hidden" name="period_id" value="<?php echo (int)$period['id']; ?>">
+                                                    <button type="submit" class="btn btn-outline-primary btn-sm">Enviar código</button>
+                                                </form>
+                                                <form method="post" action="index.php?route=accounting/periods/open" class="d-flex gap-2">
+                                                    <input type="hidden" name="csrf_token" value="<?php echo csrf_token(); ?>">
+                                                    <input type="hidden" name="period_id" value="<?php echo (int)$period['id']; ?>">
+                                                    <input type="text" name="open_code" class="form-control form-control-sm" placeholder="Código" required>
+                                                    <button type="submit" class="btn btn-success btn-sm">Abrir</button>
+                                                </form>
+                                            </div>
                                         <?php endif; ?>
                                     </td>
                                 </tr>

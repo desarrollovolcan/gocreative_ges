c68d7640b1279f47a5cad47dd4ed0fa2c968c1a4
desarrollovolcan <eisla@fvolcan.cl>
2026-01-07 21:20:01 -0300

    Use ficha template for briefs report

---
+++ informes/briefs_create.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . '/report-base.php';
+
+generate_form_report([
+    'title' => 'Informe de brief comercial',
+    'source' => 'crm/briefs',
+    'template' => 'informeFicha.php',
+]);

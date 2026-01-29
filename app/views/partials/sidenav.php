70239a681c2b4a88cdf85c28157c7942c6a1e2c0
desarrollovolcan <eisla@fvolcan.cl>
2026-01-29 16:52:22 -0300

    Reorganize products and materials menu for production

--- app/views/partials/sidenav.php
+++ app/views/partials/sidenav.php
@@ -84,26 +84,6 @@ $logoSmallBlack = $companySettings['logo_black'] ?? 'assets/images/logo-sm.png';
                                     <span class="menu-text">Panel CRM</span>
                                 </a>
                             </li>
-                            <li class="side-nav-item">
-                                <a href="index.php?route=crm/reports" class="side-nav-link">
-                                    <span class="menu-text">Reportes &amp; Insights</span>
-                                </a>
-                            </li>
-                            <li class="side-nav-item">
-                                <a href="index.php?route=crm/briefs" class="side-nav-link">
-                                    <span class="menu-text">Briefs Comerciales</span>
-                                </a>
-                            </li>
-                            <li class="side-nav-item">
-                                <a href="index.php?route=crm/orders" class="side-nav-link">
-                                    <span class="menu-text">Órdenes de Venta</span>
-                                </a>
-                            </li>
-                            <li class="side-nav-item">
-                                <a href="index.php?route=crm/renewals" class="side-nav-link">
-                                    <span class="menu-text">Renovaciones</span>
-                                </a>
-                            </li>
                         </ul>
                     </div>
                 </li>
@@ -199,13 +179,6 @@ $logoSmallBlack = $companySettings['logo_black'] ?? 'assets/images/logo-sm.png';
                                     <span class="menu-text">Renovaciones</span>
                                 </a>
                             </li>
-                            <?php if ($hasPermission('services_view')): ?>
-                                <li class="side-nav-item">
-                                    <a href="index.php?route=services" class="side-nav-link">
-                                        <span class="menu-text">Servicios</span>
-                                    </a>
-                                </li>
-                            <?php endif; ?>
                             <?php if ($hasPermission('tickets_view')): ?>
                                 <li class="side-nav-item">
                                     <a href="index.php?route=tickets" class="side-nav-link">
@@ -362,14 +335,14 @@ $logoSmallBlack = $companySettings['logo_black'] ?? 'assets/images/logo-sm.png';
                     </a>
                 </li>
             <?php endif; ?>
-            <?php if ($hasCompany && $canAccessAny(['products_view', 'products_edit', 'suppliers_view', 'suppliers_edit', 'purchases_view', 'purchases_edit', 'sales_view', 'sales_edit'])): ?>
-                <li class="side-nav-title">Inventario</li>
+            <?php if ($hasCompany && $canAccessAny(['products_view', 'products_edit', 'suppliers_view', 'suppliers_edit', 'purchases_view', 'purchases_edit', 'product_families_view', 'product_subfamilies_view', 'production_view', 'production_edit'])): ?>
+                <li class="side-nav-title">Productos y materias primas</li>
                 <li class="side-nav-item">
                     <a data-bs-toggle="collapse" href="#sidebarInventory" aria-expanded="false" aria-controls="sidebarInventory" class="side-nav-link">
                         <span class="menu-icon"><i data-lucide="package"></i></span>
                         <span class="menu-label">
-                            <span class="menu-text">Productos</span>
-                            <span class="menu-caption">Compras y stock</span>
+                            <span class="menu-text">Productos y materias primas</span>
+                            <span class="menu-caption">Compras y gestión productiva</span>
                         </span>
                         <span class="menu-arrow"></span>
                     </a>
@@ -396,6 +369,18 @@ $logoSmallBlack = $companySettings['logo_black'] ?? 'assets/images/logo-sm.png';
                                     </a>
                                 </li>
                             <?php endif; ?>
+                            <?php if ($hasPermission('production_view')): ?>
+                                <li class="side-nav-item">
+                                    <a href="index.php?route=production/inputs" class="side-nav-link">
+                                        <span class="menu-text">Consumos para producción</span>
+                                    </a>
+                                </li>
+                                <li class="side-nav-item">
+                                    <a href="index.php?route=production/expenses" class="side-nav-link">
+                                        <span class="menu-text">Gastos para producción</span>
+                                    </a>
+                                </li>
+                            <?php endif; ?>
                             <?php if ($hasPermission('product_families_view')): ?>
                                 <li class="side-nav-item">
                                     <a href="index.php?route=maintainers/product-families" class="side-nav-link">
@@ -410,15 +395,32 @@ $logoSmallBlack = $companySettings['logo_black'] ?? 'assets/images/logo-sm.png';
                                     </a>
                                 </li>
                             <?php endif; ?>
-                            <?php if ($hasPermission('sales_view')): ?>
+                        </ul>
+                    </div>
+                </li>
+            <?php endif; ?>
+            <?php if ($hasCompany && $canAccessAny(['production_view', 'production_edit'])): ?>
+                <li class="side-nav-title">Producción</li>
+                <li class="side-nav-item">
+                    <a data-bs-toggle="collapse" href="#sidebarProduction" aria-expanded="false" aria-controls="sidebarProduction" class="side-nav-link">
+                        <span class="menu-icon"><i data-lucide="factory"></i></span>
+                        <span class="menu-label">
+                            <span class="menu-text">Producción</span>
+                            <span class="menu-caption">Costos y stock final</span>
+                        </span>
+                        <span class="menu-arrow"></span>
+                    </a>
+                    <div class="collapse" id="sidebarProduction">
+                        <ul class="sub-menu">
+                            <?php if ($hasPermission('production_view')): ?>
                                 <li class="side-nav-item">
-                                    <a href="index.php?route=sales" class="side-nav-link">
-                                        <span class="menu-text">Ventas</span>
+                                    <a href="index.php?route=production" class="side-nav-link">
+                                        <span class="menu-text">Órdenes de producción</span>
                                     </a>
                                 </li>
                                 <li class="side-nav-item">
-                                    <a href="index.php?route=pos" class="side-nav-link">
-                                        <span class="menu-text">Punto de venta</span>
+                                    <a href="index.php?route=production/stock" class="side-nav-link">
+                                        <span class="menu-text">Stock producido</span>
                                     </a>
                                 </li>
                             <?php endif; ?>
